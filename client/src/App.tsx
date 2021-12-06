7ba495080211ac758ec76399edff19b5349f7471
Raghav Saraf <40730714+sarafraghav@users.noreply.github.com>
2021-12-06 18:59:16 +0530

    Works lmao

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,24 +16,25 @@ import { useState } from 'react'
 function App() {
   const { enableWeb3, isWeb3Enabled, isAuthenticated, user } = useMoralis();
 // Moralis
-
+   console.log(isAuthenticated)
   if (!isAuthenticated) {
     return <Landing />
   }
+
 
   if(!isWeb3Enabled){
      enableWeb3()
   }
 
-   console.log("hello")
+    // {/* @ts-ignore */}
+    // <h1>Welcome {user.get("ethAddress")}</h1>
+     //<LogoutButton />
+     //<TransferButton amount={10} address="0x479327C7658AeBFa9F777B1B79D9353C7387e266" />
+    //<DisplayTransaction />
 
   return (
     <div>
-      {/* @ts-ignore */}
-      <h1>Welcome {user.get("ethAddress")}</h1>
-      <LogoutButton />
-      <TransferButton amount={10} address="0x479327C7658AeBFa9F777B1B79D9353C7387e266" />
-     <DisplayTransaction />
+    
   
       <Routes>
       <Route path='/dashboard' element={<Dashboard />}></Route>
--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
+import { AuthenticateButton } from "../components/Auth";
 
 const Landing = () => {
   return (
     <div>
       <h1>Landing Page</h1>
+      <AuthenticateButton />
       <Link to='dashboard'>Go to Dashboard</Link>
     </div>
   )
