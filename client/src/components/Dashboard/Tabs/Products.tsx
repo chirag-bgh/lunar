da230009870aa2860667bc6fd6209bdbf984cdc7
Shadow7258 <pranav.rdoshi@gmail.com>
2021-12-07 10:48:20 +0530

    fix: create product button

--- client/src/components/Dashboard/Tabs/Products.tsx
+++ client/src/components/Dashboard/Tabs/Products.tsx
@@ -1,33 +1,28 @@
-
-
-
-import {CreateProduct,FetchProduct} from "./../../../components/Products";
-
+import { CreateProduct, FetchProduct } from './../../../components/Products'
 
 const Products = () => {
-
-
   return (
     <div className='w-full'>
       <div className='w-full flex flex-col justify-between items-start'>
         <h2 className='text-3xl underline font-medium'>Products</h2>
-        <div className='w-16 h-full rounded-sm bg-primary flex justify-center items-center'>
-         <CreateProduct name='Raghav' price = {10}/>
-        </div>
       </div>
-      
-        <div className='flex h-8 justify-end items-between gap-1 mt-12'>
-        <input
-          className='h-full w-72 bg-dark rounded-sm text-white pl-2 outline-none text-sm'
-          type='text'
-          placeholder='Search by product, ID, or subscribers'
-        />
-        <div className='w-16 h-full rounded-sm bg-primary flex justify-center items-center'>
-          Search
-        </div>
 
+      <div className='flex h-8 justify-between items-between gap-1 mt-12'>
+        <div className='px-5 h-full rounded-sm bg-primary flex justify-center items-center'>
+          <CreateProduct name='Raghav' price={10} />
+        </div>
+        <div className='flex justify-center items-center'>
+          <input
+            className='h-full w-72 bg-dark rounded-sm text-white pl-2 outline-none text-sm'
+            type='text'
+            placeholder='Search by product, ID, or subscribers'
+          />
+          <div className='w-16 h-full rounded-sm bg-primary flex justify-center items-center'>
+            Search
+          </div>
+        </div>
       </div>
-      
+
       <table className='text-white bg-dark w-full mt-5 rounded-lg'>
         <tr className='border-gray-500 border-b-2'>
           <th>Product</th>
@@ -36,7 +31,9 @@ const Products = () => {
           <th>Category</th>
         </tr>
         <tr className='h-80'>
-          <td><FetchProduct /></td>
+          <td>
+            <FetchProduct />
+          </td>
         </tr>
       </table>
     </div>
