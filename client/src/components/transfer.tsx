647bdf51b235ad04cdd9c42a09750778368d05a3
Raghav Saraf <40730714+sarafraghav@users.noreply.github.com>
2021-12-08 09:49:09 +0530

    Transfer Merge

--- client/src/components/transfer.tsx
+++ client/src/components/transfer.tsx
@@ -1,25 +1,19 @@
 import { useWeb3Transfer,useMoralis,useMoralisQuery } from "react-moralis";
 import Moralis from "moralis";
 
-const Transfer = ({amount,address}:{amount:number, address:string}) => {
-    const {fetch} = useWeb3Transfer({
-      amount: Moralis.Units.ETH(amount),
-      receiver: address,
-      type: "native",
-    });
+const Transfer = ({objectID}:{objectID:string}) => {
+    const { data } = useMoralisQuery('Products', (query) =>
+    query.equalTo('objectID', objectID)
+  )
+  const {fetch} = useWeb3Transfer({
+    amount: Moralis.Units.ETH(10),
+    receiver: "Hi",
+    type: "native",
+  });
     
     return (fetch())
   }
 
-  const TransferButton = ({objectID}:{objectID:string}) => {
-    const { data } = useMoralisQuery('Products', (query) =>
-    query.equalTo('objectID', objectID)
-  )
-  let json = JSON.stringify(data, null, 2)
-    return (<div>
-      <button onClick={Transfer({10,'Hi'})}>Transfer</button>
-    </div>)
-  }
 
   
   const DisplayTransaction = () => {
