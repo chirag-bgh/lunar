12d8ff794b178b8568b6485cded2d88f9d8fccd5
Shadow7258 <pranav.rdoshi@gmail.com>
2021-12-08 15:17:42 +0530

    fix balance thing

--- client/src/components/Auth/AuthManager.tsx
+++ client/src/components/Auth/AuthManager.tsx
@@ -1,35 +1,34 @@
-import { useMoralis } from "react-moralis";
+import { useMoralis } from 'react-moralis'
 
 const AuthenticateButton = () => {
-  const { authenticate } = useMoralis();
+  const { authenticate } = useMoralis()
 
-  return <button onClick={() => authenticate()}>Authenticate</button>;
-
-};
+  return <button onClick={() => authenticate()}>Authenticate</button>
+}
 
 const UserChecker = () => {
-  const { user,setUserData, web3 } = useMoralis();
-  
-  let x  = web3.eth.accounts.create();
-  
-  if (user.get('managed_account_priv') == undefined){
-    setUserData({managed_account_pub:x.address,managed_account_priv:x.privateKey})
+  const { user, setUserData, web3 } = useMoralis()
+
+  let x = web3.eth.accounts.create()
+
+  if (user.get('managed_account_priv') === undefined) {
+    setUserData({
+      managed_account_pub: x.address,
+      managed_account_priv: x.privateKey,
+    })
   } else {
-    console.log("bitch")
+    // console.log('bitch')
   }
 
-  console.log('User',user.get('managed_account_priv'))
+  console.log('User', user.get('managed_account_priv'))
 
   return <div></div>
-
-};
-
+}
 
 const LogoutButton = () => {
-  const { logout } = useMoralis();
+  const { logout } = useMoralis()
 
-  return <button onClick={() => logout()}>Logout</button>;
-};
+  return <button onClick={() => logout()}>Logout</button>
+}
 
-
-export { AuthenticateButton,UserChecker, LogoutButton};
+export { AuthenticateButton, UserChecker, LogoutButton }
