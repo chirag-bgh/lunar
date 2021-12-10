e69b4fb28dcf186307a5914498dd64e48eaab979
Raghav Saraf <40730714+sarafraghav@users.noreply.github.com>
2021-12-10 10:20:10 +0530

    bugs

--- client/src/components/Dashboard/Tabs/Subscriptions.tsx
+++ client/src/components/Dashboard/Tabs/Subscriptions.tsx
@@ -1,25 +1,22 @@
 import {
   CreateSubscription,
   FetchSubscription,
-} from '../../../backend/Subscription'
+} from "../../../backend/Subscription";
 
 const Subscriptions = () => {
   return (
-    <div className='w-full'>
-      <div className='w-full flex flex-col justify-between items-start'>
-        <h2 className='text-3xl underline font-medium'>Subscription Plans</h2>
+    <div className="w-full">
+      <div className="w-full flex flex-col justify-between items-start">
+        <h2 className="text-3xl underline font-medium">Subscription Plans</h2>
       </div>
-      <div className='flex h-8 justify-between items-between gap-1 mt-12'>
-        <div className='px-5 h-full rounded-sm bg-primary flex justify-center items-center'>
-          <CreateSubscription name='Shoes' price={0.4} recurrence='Monthly' />
-        </div>
-        <div className='flex justify-center items-center'>
+      <div className="flex h-8 justify-between items-between gap-1 mt-12">
+        <div className="flex justify-center items-center">
           <input
-            className='h-full w-72 bg-dark rounded-sm text-white pl-2 outline-none text-sm'
-            type='text'
-            placeholder='Search by product, ID, or subscribers'
+            className="h-full w-72 bg-dark rounded-sm text-white pl-2 outline-none text-sm"
+            type="text"
+            placeholder="Search by product, ID, or subscribers"
           />
-          <div className='w-16 h-full rounded-sm bg-primary flex justify-center items-center'>
+          <div className="w-16 h-full rounded-sm bg-primary flex justify-center items-center">
             Search
           </div>
         </div>
@@ -27,7 +24,7 @@ const Subscriptions = () => {
 
       <FetchSubscription />
     </div>
-  )
-}
+  );
+};
 
-export default Subscriptions
+export default Subscriptions;
