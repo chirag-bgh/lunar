a18bd6f8858dda5de9ad4acf62466c802cd42aec
Raghav Saraf <40730714+sarafraghav@users.noreply.github.com>
2021-12-10 17:25:14 +0530

    Settings page functioning

---
+++ client/src/backend/settings.tsx
@@ -0,0 +1,28 @@
+// Modal
+import { useState } from "react";
+import { useMoralis } from "react-moralis";
+
+export const Setting_backend = ({
+  callback,
+  webhook,
+}: {
+  callback: string;
+  webhook: string;
+}) => {
+  const { user, setUserData } = useMoralis();
+
+  return (
+    <button
+      onClick={() => {
+        setUserData({
+          callbackURL: callback,
+          webhookURL: webhook,
+        });
+        console.log("User: ", user);
+      }}
+      className="px-14 py-1 bg-primary rounded-sm flex justify-center items-center font-semibold cursor-pointer"
+    >
+      Set Configuration
+    </button>
+  );
+};
--- client/src/components/Dashboard/Tabs/Settings.tsx
+++ client/src/components/Dashboard/Tabs/Settings.tsx
@@ -1,21 +1,40 @@
-const Settings = () => {
-    return (
-        <div className="w-full">
-        <h1 className="text-3xl underline font-medium">Settings</h1>
-        <div className="flex flex-col w-full h-full justify-center items-center">
-            <div className="urls h-24 mt-10 ">
-                <h1 className="font-display text-white font-medium">Callback URL: </h1>
-                <input className="h-10 w-72 bg-dark rounded-sm text-white pl-2 outline-none text-sm" type="text" placeholder="Enter callback URL here" value="">
-                </input>
-            </div>
-        
-            <div className="bg-primary p-5 mt-2 flex justify-center items-center  h-10 gap-1 rounded-lg cursor-pointer mt-10">
-                <h1 className="text-dark font-display font-semibold">Set Configuration</h1>
-            </div>
-        </div>
-        </div>
-        
-    );
-}
+// Modal
+import { useState } from "react";
+import { useMoralis } from "react-moralis";
+import { Setting_backend } from "../../../backend/settings";
 
-export default Settings
+const Settings = () => {
+  const { user, setUserData } = useMoralis();
+
+  const [callback, setcallback] = useState("");
+  const [webhook, setwebhook] = useState("");
+
+  return (
+    <div className="w-full">
+      <h1 className="text-3xl underline font-medium">Settings</h1>
+      <div className="flex flex-col w-full h-full justify-center items-center">
+        <div className="urls h-24 mt-10 ">
+          <h1 className="font-display text-white font-medium">
+            Callback URL:{" "}
+          </h1>
+          <input
+            className="h-10 w-72 bg-dark rounded-sm text-white pl-2 outline-none text-sm"
+            type="text"
+            placeholder="Enter Callback URL here"
+            onChange={(e) => setcallback(e.target.value)}
+          ></input>
+          <h1 className="font-display text-white font-medium">Webhook URL: </h1>
+          <input
+            className="h-10 w-72 bg-dark rounded-sm text-white pl-2 outline-none text-sm"
+            type="text"
+            placeholder="Enter Webhook URL here"
+            onChange={(e) => setwebhook(e.target.value)}
+          ></input>
+          <Setting_backend callback={callback} webhook={webhook} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
