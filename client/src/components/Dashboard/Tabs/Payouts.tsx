2681bc4e13f848836e97831cb404d7dccce97bde
Shadow7258 <pranav.rdoshi@gmail.com>
2021-12-10 10:05:39 +0530

    removed react hot toast

--- client/src/components/Dashboard/Tabs/Payouts.tsx
+++ client/src/components/Dashboard/Tabs/Payouts.tsx
@@ -4,15 +4,12 @@ import { useState } from 'react'
 import { FetchWithdrawals, Withdraw } from '../../../backend/withdraw'
 import { useMoralis } from 'react-moralis'
 
-import { Toaster, toast } from 'react-hot-toast'
-
 const Payouts = () => {
   const { user } = useMoralis()
   let address = user.get('ethAddress')
   const [selected, setSelected] = useState(address)
   return (
     <div className='w-full'>
-      <Toaster />
       <div className='flex flex-col justify-between items-start'>
         <h2 className='text-3xl underline font-medium'>Wallets</h2>
       </div>
@@ -26,15 +23,7 @@ const Payouts = () => {
           />
         </div>
         <button
-          onClick={() => {
-            toast('Coming Soon!', {
-              style: {
-                borderRadius: '10px',
-                background: '#333',
-                color: '#fff',
-              },
-            })
-          }}
+          disabled
           style={{ filter: 'grayscale(1) brightness(0.3)' }}
           className='bg-primary rounded-full w-24 h-24 text-7xl font-display flex justify-center items-center cursor-pointer'
         >
