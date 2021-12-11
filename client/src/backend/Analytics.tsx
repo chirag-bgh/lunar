4f6df1ffd53102c7fd6088162fca2155515d07a6
Raghav Saraf <40730714+sarafraghav@users.noreply.github.com>
2021-12-11 18:09:35 +0530

    Round Up figures

--- client/src/backend/Analytics.tsx
+++ client/src/backend/Analytics.tsx
@@ -1,5 +1,5 @@
-import { useMoralisQuery, useMoralis } from 'react-moralis'
-import TransactionClass from '../classes/TransactionClass'
+import { useMoralisQuery, useMoralis } from "react-moralis";
+import TransactionClass from "../classes/TransactionClass";
 
 import {
   CartesianGrid,
@@ -10,92 +10,92 @@ import {
   Tooltip,
   XAxis,
   YAxis,
-} from 'recharts'
+} from "recharts";
 
 export const GetRevenue = () => {
-  const { user } = useMoralis()
+  const { user } = useMoralis();
 
-  const userAddress = user.get('managed_account_pub')
+  const userAddress = user.get("managed_account_pub");
 
-  const { data } = useMoralisQuery('Transactions', (query) =>
-    query.equalTo('to_address', userAddress)
-  )
+  const { data } = useMoralisQuery("Transactions", (query) =>
+    query.equalTo("to_address", userAddress)
+  );
 
-  let json = JSON.stringify(data, null, 2)
+  let json = JSON.stringify(data, null, 2);
 
-  const transactions: TransactionClass[] = JSON.parse(json)
+  const transactions: TransactionClass[] = JSON.parse(json);
 
   //   console.log('transactions', json)
 
-  let prices: number[] = transactions.map((transaction) => transaction.amount)
+  let prices: number[] = transactions.map((transaction) => transaction.amount);
 
-  let revenue = 0
+  let revenue = 0;
 
   if (prices.length > 0) {
-    revenue = prices.reduce((prev, next) => prev + next, 0)
+    revenue = prices.reduce((prev, next) => prev + next, 0);
   }
 
   // console.log('revenue', revenue)
-  return <div>{revenue} MATIC</div>
-}
+  return <div>{revenue.toString().substring(0, 4)} MATIC</div>;
+};
 
 export const GetTransactions = () => {
-  const { user } = useMoralis()
+  const { user } = useMoralis();
 
-  const userAddress = user.get('managed_account_pub')
+  const userAddress = user.get("managed_account_pub");
 
-  const { data } = useMoralisQuery('Transactions', (query) =>
-    query.equalTo('to_address', userAddress)
-  )
+  const { data } = useMoralisQuery("Transactions", (query) =>
+    query.equalTo("to_address", userAddress)
+  );
 
-  let json = JSON.stringify(data, null, 2)
+  let json = JSON.stringify(data, null, 2);
 
-  const transactions: TransactionClass[] = JSON.parse(json)
+  const transactions: TransactionClass[] = JSON.parse(json);
 
-  return <div>{transactions.length}</div>
-}
+  return <div>{transactions.length}</div>;
+};
 
 export const DisplayChart = () => {
-  const { user } = useMoralis()
+  const { user } = useMoralis();
 
-  const userAddress = user.get('managed_account_pub')
+  const userAddress = user.get("managed_account_pub");
 
-  const { data } = useMoralisQuery('Transactions', (query) =>
-    query.equalTo('to_address', userAddress)
-  )
+  const { data } = useMoralisQuery("Transactions", (query) =>
+    query.equalTo("to_address", userAddress)
+  );
 
-  let json = JSON.stringify(data, null, 2)
+  let json = JSON.stringify(data, null, 2);
 
-  let dateObj = new Date()
+  let dateObj = new Date();
 
-  let chartData = []
+  let chartData = [];
 
-  const transactions: TransactionClass[] = JSON.parse(json)
+  const transactions: TransactionClass[] = JSON.parse(json);
 
-  let dates: Date[] = transactions.map((transaction) => transaction.createdAt)
+  let dates: Date[] = transactions.map((transaction) => transaction.createdAt);
 
   for (let i = 0; i < 7; i++) {
-    dateObj.setDate(dateObj.getDate() - 1)
+    dateObj.setDate(dateObj.getDate() - 1);
 
     let date =
       dateObj.getFullYear() +
-      '-' +
+      "-" +
       (dateObj.getMonth() + 1) +
-      '-' +
-      (dateObj.getDate() + 1)
+      "-" +
+      (dateObj.getDate() + 1);
 
-    let revenue = 0
+    let revenue = 0;
 
-    let transactionCount = 0
+    let transactionCount = 0;
 
     for (let index = 0; index < dates.length; index++) {
-      const transactionDate = dates[index]
+      const transactionDate = dates[index];
 
       if (transactionDate.toString().startsWith(date)) {
         if (transactions[index] !== undefined) {
-          revenue += transactions[index].amount
+          revenue += transactions[index].amount;
         }
-        transactionCount += 1
+        transactionCount += 1;
       }
     }
 
@@ -103,13 +103,13 @@ export const DisplayChart = () => {
       name: date,
       revenue: revenue,
       transactions: transactionCount,
-    })
+    });
   }
 
   // console.log('chartData', chartData)
 
   return (
-    <ResponsiveContainer width='100%' height='65%' className='mt-10'>
+    <ResponsiveContainer width="100%" height="65%" className="mt-10">
       <LineChart
         width={500}
         height={300}
@@ -121,19 +121,19 @@ export const DisplayChart = () => {
           bottom: 5,
         }}
       >
-        <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='name' />
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
         <Legend />
         <Line
-          type='monotone'
-          dataKey='revenue'
-          stroke='#8884d8'
+          type="monotone"
+          dataKey="revenue"
+          stroke="#8884d8"
           activeDot={{ r: 8 }}
         />
-        <Line type='monotone' dataKey='transactions' stroke='#82ca9d' />
+        <Line type="monotone" dataKey="transactions" stroke="#82ca9d" />
       </LineChart>
     </ResponsiveContainer>
-  )
-}
+  );
+};
