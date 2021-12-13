77b1554ef899de473a7fb000c4ebffc568e43b2d
Raghav Saraf <40730714+sarafraghav@users.noreply.github.com>
2021-12-13 20:43:50 +0530

    Autoswitch to polygon completed

--- client/src/components/Auth/AuthManager.tsx
+++ client/src/components/Auth/AuthManager.tsx
@@ -1,62 +1,66 @@
-import { useMoralis, useMoralisQuery } from 'react-moralis'
-import CryptoJS from 'crypto-js'
-import { useState } from 'react'
-
+import { useMoralis, useChain, useMoralisQuery } from "react-moralis";
+import CryptoJS from "crypto-js";
+import { useState } from "react";
+import Moralis from "moralis";
 // Spinner
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
-import Loader from 'react-loader-spinner'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 const AuthenticateButton = () => {
-  const { enableWeb3, isWeb3Enabled, authenticate } = useMoralis()
-  const [called, setcalled] = useState(false)
+  const { enableWeb3, isWeb3Enabled, authenticate } = useMoralis();
+  const [called, setcalled] = useState(false);
 
   return (
     <div>
       <button
         onClick={() => {
           if (!isWeb3Enabled) {
-            enableWeb3()
+            enableWeb3();
           }
-          authenticate()
-          setcalled(true)
+          authenticate();
+          setcalled(true);
         }}
-        className='rounded-sm flex justify-center items-center font-medium font-display cursor-pointer text-lg md:text-md'
+        className="rounded-sm flex justify-center items-center font-medium font-display cursor-pointer text-lg md:text-md"
       >
         {!called ? (
           <span>Authenticate</span>
         ) : (
-          <div className='flex justify-center items-center '>
+          <div className="flex justify-center items-center ">
             <span>Authenticating </span>
-            <Loader type='Puff' color='white' height={20} width={30} />
+            <Loader type="Puff" color="white" height={20} width={30} />
           </div>
         )}
       </button>
     </div>
-  )
-}
+  );
+};
 
 const UserChecker = ({
   setBalance,
   setFetched,
 }: {
-  setBalance: (arg: string) => void
-  setFetched: (arg: boolean) => void
+  setBalance: (arg: string) => void;
+  setFetched: (arg: boolean) => void;
 }) => {
-  const { user, setUserData, web3, isWeb3Enabled, enableWeb3 } = useMoralis()
-  // const { switchNetwork, chainId } = useChain()
+  const { user, setUserData, web3, isWeb3Enabled, enableWeb3 } = useMoralis();
+  const { switchNetwork, chainId, chain, account } = useChain();
 
   if (!isWeb3Enabled) {
-    enableWeb3()
+    enableWeb3();
+  }
+  // const { switchNetwork, chainId } = useChain()
+  if (chainId !== "0x13881") {
+    switchNetwork("0x13881");
   }
 
-  useMoralisQuery('PolygonTransactions', (query) => query, [], {
+  useMoralisQuery("PolygonTransactions", (query) => query, [], {
     live: true,
     onLiveCreate: (entity, all) => {
-      console.log('Polygon Transaction Edited')
-      setFetched(false)
-      return [...all, entity]
+      console.log("Polygon Transaction Edited");
+      setFetched(false);
+      return [...all, entity];
     },
-  })
+  });
 
   // useEffect(() => {
   //   switchNetwork('0x80001')
@@ -65,39 +69,39 @@ const UserChecker = ({
 
   // console.log('password: ', process.env.REACT_APP_PASSWORD)
 
-  if (user.get('encryptedKey') === undefined) {
-    let x = web3.eth.accounts.create()
+  if (user.get("encryptedKey") === undefined) {
+    let x = web3.eth.accounts.create();
 
     let encryptedKey = CryptoJS.AES.encrypt(
       x.privateKey,
       process.env.REACT_APP_PASSWORD
-    ).toString()
-    console.log('private key: ', x.privateKey)
+    ).toString();
+    console.log("private key: ", x.privateKey);
 
-    console.log('encryptedKey: ', encryptedKey)
+    console.log("encryptedKey: ", encryptedKey);
 
     var bytes = CryptoJS.AES.decrypt(
       encryptedKey,
       process.env.REACT_APP_PASSWORD
-    )
-    var decryptedData = bytes.toString(CryptoJS.enc.Utf8)
-    console.log('decryptedData: ', decryptedData)
+    );
+    var decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+    console.log("decryptedData: ", decryptedData);
 
     setUserData({
       managed_account_pub: x.address,
       encryptedKey: encryptedKey,
-    })
+    });
 
     // withdrawalAddress.addUnique(x.address),
   }
 
-  return <div></div>
-}
+  return <div></div>;
+};
 
 const LogoutButton = () => {
-  const { logout } = useMoralis()
+  const { logout } = useMoralis();
 
-  return <button onClick={() => logout()}>Logout</button>
-}
+  return <button onClick={() => logout()}>Logout</button>;
+};
 
-export { AuthenticateButton, UserChecker, LogoutButton }
+export { AuthenticateButton, UserChecker, LogoutButton };
