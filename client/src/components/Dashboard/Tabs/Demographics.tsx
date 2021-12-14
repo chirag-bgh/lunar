3d17dd48c7034a2cf83676f4216830f9803d1de7
Shadow7258 <pranav.rdoshi@gmail.com>
2021-12-14 19:00:58 +0530

    demographis design

--- client/src/components/Dashboard/Tabs/Demographics.tsx
+++ client/src/components/Dashboard/Tabs/Demographics.tsx
@@ -51,56 +51,53 @@ const Demographics = () => {
 
   return (
     <div className='w-full'>
-      <div className='w-full flex flex-col justify-between items-start'>
+      <div className='w-full flex flex-col justify-between items-start mb-20'>
         <h2 className='text-3xl underline font-medium'>Demographics</h2>
       </div>
-      <div className='flex items-center'>
-        <div className='flex flex-col justify-start items-start w-full gap-4'>
-          <div className='flex justify-start items-stretch gap-4 w-full'>
-            <div className='bg-dark w-40 py-4 min-h-fit max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-evenly items-center'>
-              <p className='my-2 text-xl'>Total Users</p>
-              <GetUsers />
-            </div>
-            <div className='bg-dark w-80 py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
-              <p className='my-2 text-xl'>Users by Country</p>
-              <GetCountries />
-              <div></div>
-            </div>
-            <div className='bg-dark w-80 py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
-              <p className='my-2 text-xl'>Users by City</p>
-              <GetCities />
-              <div></div>
-            </div>
+      <div className='flex flex-col justify-start items-start gap-4 mr-8 w-5/6'>
+        <div className='flex justify-start items-stretch gap-4 w-full'>
+          <div className='bg-dark w-1/3 py-4 min-h-fit rounded-md border-2 border-gray-600 flex flex-col justify-evenly items-center'>
+            <p className='my-2 text-xl'>Total Users</p>
+            <GetUsers />
           </div>
-          <div className='flex justify-start items-start gap-4 w-full'>
-            <div
-              style={{ width: '52rem' }}
-              className='bg-dark py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'
-            >
-              <p className='my-2 text-xl'>Top Customers</p>
-              <GetUserLeaderboard />
-              <div></div>
-            </div>
+          <div className='bg-dark w-1/3 py-4 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
+            <p className='my-2 text-xl'>Users by Country</p>
+            <GetCountries />
+            <div></div>
+          </div>
+          <div className='bg-dark w-1/3 py-4 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
+            <p className='my-2 text-xl'>Users by City</p>
+            <GetCities />
+            <div></div>
           </div>
         </div>
-        <DonutChart
-          legend={false}
-          colors={colors}
-          width='500'
-          data={chartData}
-          onMouseEnter={(item) => {
-            console.log(`mousing over: ${item.label}`)
-            return item
-          }}
-          onClick={(item, toggled) => {
-            if (toggled) {
-              console.log(`selecting: ${item.label}`)
-            } else {
-              console.log(`unselecting: ${item.label}`)
-            }
-            return item
-          }}
-        />
+        <div className='flex justify-start items-start gap-4 w-full'>
+          <div className='bg-dark w-full py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
+            <p className='my-2 text-xl'>Top Customers</p>
+            <GetUserLeaderboard />
+            <div></div>
+          </div>
+        </div>
+        <div className='rounded-md border-2 border-gray-600 bg-dark w-full py-2 max-h-80  flex flex-col justify-between items-center'>
+          <DonutChart
+            legend={false}
+            colors={colors}
+            width='500'
+            data={chartData}
+            onMouseEnter={(item) => {
+              console.log(`mousing over: ${item.label}`)
+              return item
+            }}
+            onClick={(item, toggled) => {
+              if (toggled) {
+                console.log(`selecting: ${item.label}`)
+              } else {
+                console.log(`unselecting: ${item.label}`)
+              }
+              return item
+            }}
+          />
+        </div>
       </div>
     </div>
   )
