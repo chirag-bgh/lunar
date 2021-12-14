12a3b8950ba429a895a7d1b02b0d0d6529cee8b5
Shadow7258 <pranav.rdoshi@gmail.com>
2021-12-14 18:43:29 +0530

    added purchase type chart

--- client/src/classes/Demographics.tsx
+++ client/src/classes/Demographics.tsx
@@ -1,7 +1,12 @@
+import User from './User'
+
 export default interface DemographicsClass {
   email: string
   city: string
   country: string
   createdAt: string
   totalSpent: number
+  subscriptions: number
+  purchases: number
+  user: User
 }
--- client/src/components/Dashboard/Tabs/Demographics.tsx
+++ client/src/components/Dashboard/Tabs/Demographics.tsx
@@ -5,39 +5,102 @@ import {
   GetUsers,
 } from '../../../backend/Demographics'
 
+import DonutChart from 'react-donut-chart'
+import { useMoralis, useMoralisQuery } from 'react-moralis'
+import DemographicsClass from '../../../classes/Demographics'
+
 const Demographics = () => {
+  const { user } = useMoralis()
+
+  const { data, error, isLoading } = useMoralisQuery('Demographics', (query) =>
+    query.equalTo('user', user.id)
+  )
+
+  if (error) {
+    return <span>🤯</span>
+  }
+
+  if (isLoading) {
+    return <span>🙄</span>
+  }
+
+  let json = JSON.stringify(data, null, 2)
+
+  let demographics: DemographicsClass[] = JSON.parse(json)
+
+  let subscriptionArr: number[] = demographics.map(
+    (demographic) => demographic.subscriptions
+  )
+  let purchasesArr: number[] = demographics.map(
+    (demographic) => demographic.purchases
+  )
+
+  function add(accumulator, a) {
+    return accumulator + a
+  }
+
+  const subscriptions = subscriptionArr.reduce(add, 0)
+  const purchases = purchasesArr.reduce(add, 0)
+
+  const chartData = [
+    { label: 'Subscriptions', value: subscriptions },
+    { label: 'Product Purchases', value: purchases },
+  ]
+
+  const colors = ['#82ca9d', '#a7FdFF']
+
   return (
     <div className='w-full'>
-      <div className='w-full flex flex-col justify-between items-start mb-16'>
+      <div className='w-full flex flex-col justify-between items-start'>
         <h2 className='text-3xl underline font-medium'>Demographics</h2>
       </div>
-      <div className='flex flex-col justify-start items-start w-full gap-4'>
-        <div className='flex justify-start items-stretch gap-4 w-full'>
-          <div className='bg-dark w-40 py-4 min-h-fit max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-evenly items-center'>
-            <p className='my-2 text-xl'>Total Users</p>
-            <GetUsers />
+      <div className='flex items-center'>
+        <div className='flex flex-col justify-start items-start w-full gap-4'>
+          <div className='flex justify-start items-stretch gap-4 w-full'>
+            <div className='bg-dark w-40 py-4 min-h-fit max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-evenly items-center'>
+              <p className='my-2 text-xl'>Total Users</p>
+              <GetUsers />
+            </div>
+            <div className='bg-dark w-80 py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
+              <p className='my-2 text-xl'>Users by Country</p>
+              <GetCountries />
+              <div></div>
+            </div>
+            <div className='bg-dark w-80 py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
+              <p className='my-2 text-xl'>Users by City</p>
+              <GetCities />
+              <div></div>
+            </div>
           </div>
-          <div className='bg-dark w-80 py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
-            <p className='my-2 text-xl'>Users by Country</p>
-            <GetCountries />
-            <div></div>
-          </div>
-          <div className='bg-dark w-80 py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'>
-            <p className='my-2 text-xl'>Users by City</p>
-            <GetCities />
-            <div></div>
+          <div className='flex justify-start items-start gap-4 w-full'>
+            <div
+              style={{ width: '52rem' }}
+              className='bg-dark py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'
+            >
+              <p className='my-2 text-xl'>Top Customers</p>
+              <GetUserLeaderboard />
+              <div></div>
+            </div>
           </div>
         </div>
-        <div className='flex justify-start items-start gap-4 w-full'>
-          <div
-            style={{ width: '52rem' }}
-            className='bg-dark py-4 max-h-80 rounded-md border-2 border-gray-600 flex flex-col justify-between items-center'
-          >
-            <p className='my-2 text-xl'>Top Customers</p>
-            <GetUserLeaderboard />
-            <div></div>
-          </div>
-        </div>
+        <DonutChart
+          legend={false}
+          colors={colors}
+          width='500'
+          data={chartData}
+          onMouseEnter={(item) => {
+            console.log(`mousing over: ${item.label}`)
+            return item
+          }}
+          onClick={(item, toggled) => {
+            if (toggled) {
+              console.log(`selecting: ${item.label}`)
+            } else {
+              console.log(`unselecting: ${item.label}`)
+            }
+            return item
+          }}
+        />
       </div>
     </div>
   )
