32c57ee6d24bfc50f1886c345e3806580383c128
Shadow7258 <pranav.rdoshi@gmail.com>
2022-01-17 13:24:26 +0530

    made landing page 100x better

--- client/components/Landing.tsx
+++ client/components/Landing.tsx
@@ -12,9 +12,13 @@ import { useRouter } from 'next/router'
 const Landing = ({ alertUser = false, redirect = false }) => {
   const router = useRouter()
 
+  const [scrollState, setScrollState] = useState(0)
+
   gsap.registerPlugin(ScrollTrigger)
 
   useEffect(() => {
+    document.addEventListener('scroll', () => setScrollState(window.scrollY))
+
     if (redirect) {
       router.push('/')
     }
@@ -71,23 +75,29 @@ const Landing = ({ alertUser = false, redirect = false }) => {
     //   }
     // }).fromTo(".screen", {x:0}, {x:100});
 
-    gsap.fromTo(
-      '.card',
-      {
-        opacity: 0,
-        y: -40,
-      },
-      {
-        opacity: 1,
-        y: 0,
-        stagger: 0.1,
-      }
-    )
+    setTimeout(() => {
+      gsap.fromTo(
+        '.card',
+
+        {
+          opacity: 0,
+          y: -40,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          stagger: 0.1,
+        }
+      )
+    }, 200)
   }, [alertUser, redirect])
 
   return (
     <div>
-      <section className='top w-screen h-screen bg-dark flex flex-col overflow-x-hidden'>
+      <section
+        className='top w-screen h-screen bg-dark flex flex-col overflow-x-hidden'
+        id='section1'
+      >
         <div className='flex justify-between items-center h-16 w-full mt-3 px-7'>
           <div className='flex justify-center items-center gap-1 mb-1 z-10'>
             <img
@@ -127,20 +137,20 @@ const Landing = ({ alertUser = false, redirect = false }) => {
           </div>
         </div>
 
-        <div className='flex flex-col w-screen h-1/2 justify-center items-center font-display p-4 z-10'>
-          <h1 className='text-bold text-3xl md:text-3xl md:mt-12 mt-48 text-white align-middle text-center mb-7'>
-            Accepting payments with crypto, now easier than ever
+        <div className='flex flex-col w-screen h-screen md:h-1/2 justify-center items-center font-display p-4 z-10'>
+          <h1 className='text-bold text-4xl md:text-4xl md:mt-12 mt-48 text-white align-middle text-center mb-7'>
+            Accepting recurring crypto payments, now easier than ever.
           </h1>
-          <div className='flex justify-center items-center gap-4'>
+          <div className='flex justify-center items-center gap-4 w-full md:w-auto'>
             <a
               href='https://www.getrevue.co/profile/paylunar'
               target='_blank'
               rel='noreferrer'
-              className='bg-primary p-2 rounded-lg mb-44 mt-4 z-10 cursor-pointer'
+              className='bg-primary p-2 rounded-lg mb-44 mt-4 z-10 cursor-pointer w-4/5 md:w-auto'
             >
-              <h1 className='text-dark font-bold text-xs md:text-sm px-2 py-1 flex justify-center items-center mt-0.5'>
-                Join Waitlist
-              </h1>
+              <span className='text-dark font-bold text-xs md:text-sm px-2 py-1 flex justify-center items-center mt-0.5'>
+                Join Waitlist!
+              </span>
             </a>
           </div>
         </div>
@@ -154,7 +164,7 @@ const Landing = ({ alertUser = false, redirect = false }) => {
           alt='glow'
           className='absolute top-0 -left-12'
         />
-        <div className='absolute flex w-screen h-screen justify-center items-center'>
+        <div className='absolute w-screen h-screen justify-center items-center hidden md:flex'>
           <img
             src='/LandingAssets/screen.png'
             alt='background'
@@ -175,65 +185,70 @@ const Landing = ({ alertUser = false, redirect = false }) => {
           />
         </div>
       </section>
-      <section className='w-screen h-full bg-dark p-12 pb-24 first-trigger flex flex-col justify-center gap-16 items-center'>
-        <h1 className='font-display text-3xl md:text-7xl font-bold text-center'>
+      <section
+        className='w-screen h-full md:h-screen bg-dark p-12 pb-24 first-trigger flex flex-col justify-evenly items-center'
+        id='section2'
+      >
+        <h2 className='font-display mb-12 md:mb-0 text-3xl md:text-6xl font-bold text-left md:text-center'>
           Everything you need to accept cryptocurrency payments
-        </h1>
-        <div className=' flex justify-center items-start flex-wrap gap-12'>
-          <div className=' fade w-52 flex flex-col'>
+        </h2>
+        <div className='flex justify-start items-start flex-wrap gap-12'>
+          <div className=' fade md:w-52 flex flex-col'>
             <img
               src='/LandingAssets/icon1.svg'
               alt='Fast'
-              className='w-12 mb-2'
+              className='hidden md:block w-12 mb-2'
             />
-            <h1 className='h1 font-display font-bold italic text-xl mb-4'>
-              <span className='font-medium not-italic'>Get setup: </span>{' '}
+            <h4 className='font-display font-bold md:italic text-xl mb-4'>
+              <span className='not-italic font-bold md:font-medium'>
+                Get setup:{' '}
+              </span>{' '}
               Lightning fast
-            </h1>
+            </h4>
             <p>
               With LunarPay, all you need to get setup as a seller is a Metamask
               wallet.{' '}
             </p>
             <p>No emails. No spam. Forever.</p>
           </div>
-          <div className=' fade w-52 flex flex-col'>
+          <div className='fade md:w-52 flex flex-col'>
             <img
               src='/LandingAssets/icon4.svg'
               alt='Fast'
-              className='w-12 mb-2'
+              className='w-12 mb-2 hidden md:block'
             />
-            <h1 className='h1 font-display font-bold  text-xl mb-4'>
+            <h4 className='font-display font-bold  text-xl mb-4'>
               Recurring Payments: Yes, with crypto.
-            </h1>
+            </h4>
             <p>
               With Smart Invoices, customers can receive notifications to their
               email address whenever their next subscription fee is due.
               Re-subscribe with the click of a button.
             </p>
           </div>
-          <div className='fade  w-52 flex flex-col'>
+          <div className='fade md:w-52 flex flex-col'>
             <img
               src='/LandingAssets/icon2.svg'
               alt='Fast'
-              className='w-12 mb-2'
+              className='w-12 mb-2 hidden md:block'
             />
-            <h1 className='h1 font-display font-bold  text-xl mb-4'>
+            <h4 className='font-display font-bold  text-xl mb-4'>
               Withdrawals to Metamask
-            </h1>
+            </h4>
             <p>
               Withdraw earnings to your Metamask wallet at minimal GAS fees.
               It’s that easy.{' '}
             </p>
           </div>
-          <div className=' fade w-52 flex flex-col'>
+          <div className=' fade md:w-52 flex flex-col'>
             <img
               src='/LandingAssets/icon3.svg'
               alt='Fast'
-              className='w-12 mb-2'
+              className='hidden md:block w-12 mb-2'
             />
-            <h1 className='h1 font-display font-bold  text-xl mb-4'>
+            <h4 className='font-display font-bold  text-xl mb-4'>
               One click deployment: Couldn’t get easier{' '}
-            </h1>
+            </h4>
             <p>
               With every product that you enlist on LunarPay, you receive an
               HTTPS endpoint. This re-directs customers directly to our payment
@@ -242,49 +257,49 @@ const Landing = ({ alertUser = false, redirect = false }) => {
           </div>
         </div>
       </section>
-      <section className='trigger h-screen'>
-        <div className='absolute grid grid-cols-3 place-items-center text-center md:gap-24 gap-12 w-screen h-screen'>
-          <h1 className='fade-in font-semibold md:text-2xl text-sm text-gray-600 pl-10 md:pl-0'>
+      <section className='trigger h-screen' id='section3'>
+        <div className='absolute grid grid-cols-3 place-items-center text-center md:gap-24 gap-12 w-screen h-screen p-5 md:p-2'>
+          <h5 className='fade-in font-display font-semibold md:text-2xl text-sm text-gray-600 pl-10 md:pl-0'>
             Customer Management
-          </h1>
-          <h1 className='fade-in font-semibold md:text-2xl text-sm text-gray-600 '>
+          </h5>
+          <h5 className='fade-in font-display font-semibold md:text-2xl text-sm text-gray-600 '>
             One-time charges
-          </h1>
-          <h1 className='fade-in font-semibold md:text-2xl text-sm text-gray-600'>
+          </h5>
+          <h5 className='fade-in font-display font-semibold md:text-2xl text-sm text-gray-600'>
             Custom callbacks
-          </h1>
-          <h1 className='fade-in font-semibold md:text-2xl text-sm text-gray-600'>
+          </h5>
+          <h5 className='fade-in font-display font-semibold md:text-2xl text-sm text-gray-600'>
             Hosted payment pages
-          </h1>
+          </h5>
 
-          <h1 className='font-display text-black text-4xl md:text-7xl font-bold'>
+          <h2 className='font-display text-black text-4xl md:text-7xl font-bold'>
             And more
-          </h1>
+          </h2>
 
-          <h1 className='fade-in font-semibold md:text-2xl text-sm text-gray-600'>
+          <h5 className='fade-in font-display font-semibold md:text-2xl text-sm text-gray-600'>
             Invoicing
-          </h1>
-          <h1 className='fade-in font-semibold md:text-2xl text-sm text-gray-600'>
+          </h5>
+          <h5 className='fade-in font-display font-semibold md:text-2xl text-sm text-gray-600'>
             Custom Webhooks
-          </h1>
-          <h1 className='fade-in font-semibold md:text-2xl text-sm text-gray-600'>
+          </h5>
+          <h5 className='fade-in font-display font-semibold md:text-2xl text-sm text-gray-600'>
             Comprehensive Analytics
-          </h1>
-          <h1 className='fade-in font-semibold md:text-2xl text-sm text-gray-600'>
+          </h5>
+          <h5 className='fade-in font-display font-semibold md:text-2xl text-sm text-gray-600'>
             Subscription Management
-          </h1>
+          </h5>
         </div>
       </section>
-      <section className='w-screen h-screen bg-dark relative m-0'>
+      <section className='w-screen h-screen bg-dark relative m-0' id='section4'>
         <div className='div w-screen h-screen flex flex-col justify-start items-center'>
           <img
             src='/LandingAssets/polygon.svg'
             alt='PolygonLogo'
             className='w-1/5 mt-24 pb-5'
           />
-          <h1 className='font-display md:text-7xl text-2xl font-bold mb-24'>
+          <h3 className='font-display md:text-7xl text-2xl font-bold mb-24'>
             Powered by Polygon.
-          </h1>
+          </h3>
           <p className='w-3/5 font-display text-semibold text-md md:text-2xl font-medium flex-wrap'>
             On release, <span className='font-bold'>Lunar</span>Pay will be
             deployed on Polygon PoS blockchain, making GAS fees cheap while also
@@ -292,13 +307,12 @@ const Landing = ({ alertUser = false, redirect = false }) => {
           </p>
         </div>
       </section>
-      <section className=' h-80 w-screen bg-background flex justify-center items-center'>
-        <h1 className=' font-display font-medium tracking-wide md:text-xl text-lg p-4 text-center'>
-          A project proudly made{' '}
-          <a href='https://github.com/lunarpay' className=' underline'>
-            open-source
-          </a>{' '}
-          by{' '}
+      <section
+        className='h-80 w-screen bg-background flex justify-center items-center'
+        id='footer'
+      >
+        <h3 className=' font-display font-medium tracking-wide md:text-xl text-lg p-4 text-center'>
+          A project proudly made by{' '}
           <a href='https://twitter.com/aaryadoestech' className='text-primary'>
             @aaryadoestech
           </a>
@@ -310,7 +324,7 @@ const Landing = ({ alertUser = false, redirect = false }) => {
           <a href='https://twitter.com/RaghavSaraf17' className='text-primary'>
             @RaghavSaraf17
           </a>
-        </h1>
+        </h3>
       </section>
     </div>
   )
@@ -333,9 +347,9 @@ const Card = ({ imageSrc, price, classname }: any) => {
         classname
       }
     >
-      <h1 className='title font-display font-bold text-2xl mr-auto'>
-        {price} <span className='text-primary'>ETH</span>
-      </h1>
+      <h4 className='title text-white font-display font-bold text-2xl mr-auto'>
+        {price} <span className='text-primary'>MATIC</span>
+      </h4>
       <img
         src={imageSrc}
         alt='shoe'
@@ -347,14 +361,14 @@ const Card = ({ imageSrc, price, classname }: any) => {
         }}
       />
       <div className='flex flex-col justify-center items-center bg-primary rounded-lg text-dark w-full h-24'>
-        <h1 className='font-display text-dark text-xl font-bold mt-8'>
+        <h5 className='font-display text-dark text-xl font-bold mt-8'>
           Pay in crypto
-        </h1>
+        </h5>
         <div className='flex text-sm flex-shrink text-dark font-medium mb-1 mt-auto justify-center items-center'>
           {/* <IoMdMoon /> */}
-          <h1 className='text-dark'>
+          <h5 className='text-dark'>
             <span className='font-bold'>Lunar</span>Pay
-          </h1>
+          </h5>
         </div>
       </div>
     </div>
