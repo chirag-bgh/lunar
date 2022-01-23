dfa7257b2347f85f5c881d6de1868bcfa475d50b
Aarya Gadekar <aaryagadekar01@gmail.com>
2022-01-23 10:40:56 +0530

    ENS Domain support indicator

--- client/components/Dashboard/Tabs/Payouts.tsx
+++ client/components/Dashboard/Tabs/Payouts.tsx
@@ -36,8 +36,17 @@ const Payouts = ({
 
   return (
     <div className='w-full'>
-      <div className='flex flex-col justify-between items-start'>
+      <div className='flex flex items-center'>
         <h2 className='text-3xl underline font-medium'>Wallets</h2>
+        <div className="bg-transparent text-center py-4 lg:px-4 cursor-pointer">
+          <a href="https://app.ens.domains" target="_blank">
+        <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none rounded-full inline-flex" role="alert">
+          <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-2 text-xs font-bold mr-3"></span>
+          <span className="font-semibold mr-2 text-left flex-auto">We support ENS domains! </span>
+          <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+        </div>
+        </a>
+      </div>}
       </div>
 
       <div className='flex flex-wrap gap-8 items-center'>
